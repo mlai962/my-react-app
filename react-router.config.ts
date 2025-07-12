c973806295ccc56bdf01fff7b3187a7600ee9424
Matthew <huatao.lai@gmail.com>
2025-07-12 21:53:58 +1000

    fix: dynamically switch base url in dev and prod

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from "@react-router/dev/config";
 
+const devBase = "/";
+const prodBase = "/my-react-app";
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  prerender: true,
+
+  // Must begin with Vite’s `base` value for your dev server ("/")
+  // and with the subpath you’ll use in production ("/my-react-app")
+  basename: process.env.NODE_ENV === "development" ? devBase : prodBase,
 } satisfies Config;
